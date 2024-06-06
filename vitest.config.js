8b99feaa5281156fd67a72a157999bf3e8a565bb
Gokul K <gokulk@Gokuls-MacBook-Air.local>
2024-06-06 17:24:05 +0530

    Adding vitest coverage using github workflow

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   coverage: {
     enabled: true,
+    reporter: ["text", "json-summary", "json", "json-summary-compare-path"],
+    reportOnFailure: true,
   },
   test: {
     environment: "jsdom",
